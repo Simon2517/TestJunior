8c4da893294830f62f1d7db153be2d4ca8193a9f
bono <bono@BONO-LPT>
2022-01-25 14:19:37 +0100

    End of Documentation

--- TestJunior/Controllers/JuniorTest.cs
+++ TestJunior/Controllers/JuniorTest.cs
@@ -69,59 +69,71 @@ namespace TestJunior.Controllers
         }
 
         /// <summary>
-        /// returns a Brand, taking as input an Id 
-        /// using an anonimous to generate the properties
+        /// returns a Brand with relative view requests, taking as input an Id 
         /// </summary>
         [HttpGet("BrandDetail/{id}")]
         public IActionResult GetBrandDetail(int id)
         {
-
+            ///validation for Id <=0 or Id not found in the table
             if (id <= 0)
                 return BadRequest("invalid Id");
-
-            var Categs= from prod in _ctx.Product
-                        join prodCat in _ctx.ProductCategories on prod.ProductId equals prodCat.ProductId
-                        join cat in _ctx.Category on prodCat.CategoryId equals cat.Id
-                        where prod.BrandId == id
-                        group cat by new { Id=cat.Id, Name=cat.Name } into Categories
-                        select new Category{
-                            Id=Categories.Key.Id,
-                            Name=Categories.Key.Name,
-                            NumOfProducts=Categories.Count()};
+            if (_ctx.Brand.FirstOrDefault(x => x.Id == id)==null)
+                return BadRequest("Id not in table");
+            else { 
+            ///linq query to get the categories associated with the Brand and the number of products per category
+                var Categs= from prod in _ctx.Product
+                            join prodCat in _ctx.ProductCategories on prod.ProductId equals prodCat.ProductId
+                            join cat in _ctx.Category on prodCat.CategoryId equals cat.Id
+                            where prod.BrandId == id
+                            group cat by new { Id=cat.Id, Name=cat.Name } into Categories
+                            select new Category{
+                                Id=Categories.Key.Id,
+                                Name=Categories.Key.Name,
+                                NumOfProducts=Categories.Count()};
             
-            var Brands = _ctx.Brand
-                            .Include(b => b.Products)
-                                .ThenInclude(p => p.ProdsCategories)
-                                    .ThenInclude(pc => pc.Category)
-                            .Select(brand => new 
+                var Brands = _ctx.Brand
+                        .Include(b => b.Products)
+                            .ThenInclude(p => p.ProdsCategories)
+                                .ThenInclude(pc => pc.Category)
+                        .Select(brand => new 
+                        {
+                            Id = brand.Id,
+                            Name = brand.BrandName,
+                            NumOfProducts = brand.Products.Count(),
+                            NumOfInforequests = brand.Products.SelectMany(prods => prods.InfoRequests).Count(),
+                            ListOfProds = brand.Products.Select(prod => new 
                             {
-                                Id = brand.Id,
-                                Name = brand.BrandName,
-                                NumOfProducts = brand.Products.Count(),
-                                NumOfInforequests = brand.Products.SelectMany(prods => prods.InfoRequests).Count(),
-                                ListOfProds = brand.Products.Select(prod => new 
-                                {
-                                    ProductId = prod.ProductId,
-                                    ProductName = prod.Name,
-                                    NumOfInforequest = prod.InfoRequests.Count()
-                                }),
-                                ListOfCategs = Categs.ToList()
-                            })
-                            .Where(brand=>brand.Id == id);
+                                ProductId = prod.ProductId,
+                                ProductName = prod.Name,
+                                NumOfInforequest = prod.InfoRequests.Count()
+                            }),
+                            ListOfCategs = Categs.ToList()
+                        })
+                        .Where(brand=>brand.Id == id);
 
             
-            return Ok(Brands);
+                return Ok(Brands);
+            }
         }
 
+        /// <summary>
+        /// returns a Product with relative view requests, taking as input an Id 
+        /// </summary>
         [HttpGet("ProductDetail/{id}")]
-        //
         public IActionResult GetProductDetail(int id)
         {
-            var Products = _ctx.Product
+            ///validation for Id <=0 or Id not found in the table
+            if (id <= 0)
+                return BadRequest("invalid Id");
+            if (_ctx.Brand.FirstOrDefault(x => x.Id == id) == null)
+                return BadRequest("Id not in table");
+            else
+            {
+                var Products = _ctx.Product
                     .Include(product => product.ProdsCategories)
-                          .ThenInclude(pc => pc.Category)
+                            .ThenInclude(pc => pc.Category)
                     .Include(p => p.InfoRequests)
-                          .ThenInclude(ir => ir.User)
+                            .ThenInclude(ir => ir.User)
                     .Select(prod => new ProductDetail
                     {
                         ProductId = prod.ProductId,
@@ -129,65 +141,79 @@ namespace TestJunior.Controllers
 
                         Catagories = prod.ProdsCategories.Select(prodCat => new Category
                         {
-                            Id=prodCat.CategoryId,
-                            Name=prodCat.Category.Name
+                            Id = prodCat.CategoryId,
+                            Name = prodCat.Category.Name
                         }),
                         NumberOfGuestRequest = prod.InfoRequests.Where(x => x.UserId == null).Count(),
                         NumberOfUserRequest = prod.InfoRequests.Where(x => x.UserId != null).Count(),
-                        Requests=prod.InfoRequests.Select(info=>new PolishedInfoRequest
+                        Requests = prod.InfoRequests.Select(info => new PolishedInfoRequest
                         {
-                            InfoId=info.Id,
-                            Name=info.UserId==null ? info.Name : info.User.Name,
-                            LastName=info.UserId==null ? info.LastName : info.User.LastName,
-                            NumOfReplies=info.InfoRequestReplies.Count(),
-                            LastDate=info.InfoRequestReplies.Max(ir=>ir.InsertedDate)
+                            InfoId = info.Id,
+                            Name = info.UserId == null ? info.Name : info.User.Name,
+                            LastName = info.UserId == null ? info.LastName : info.User.LastName,
+                            NumOfReplies = info.InfoRequestReplies.Count(),
+                            LastDate = info.InfoRequestReplies.Max(ir => ir.InsertedDate)
                         })
                         .OrderByDescending(info => info.LastDate)
                     })
                     .Where(p => p.ProductId == id);
 
-            return Ok(Products);
+                return Ok(Products);
+            }
         }
-
+        /// <summary>
+        /// returns a InfoRequest with relative view requests, taking as input an Id 
+        /// </summary>
         [HttpGet("InfoRequestDetail/{id}")]
         public IActionResult GetInfoRequestDetail(int id)
         {
-            var InfoRequests =_ctx.InfoRequest
-                                .Include(info=>info.Product)
-                                    .ThenInclude(p=>p.Brand)
-                                .Include(info=>info.InfoRequestReplies)
-                                    .ThenInclude(reply=>reply.Account)
-                                .Include(info=>info.Nation)
+            ///validation for Id <=0 or Id not found in the table
+            if (id <= 0)
+                return BadRequest("invalid Id");
+            if (_ctx.Brand.FirstOrDefault(x => x.Id == id) == null)
+                return BadRequest("Id not in table");
+            else
+            {
+                var InfoRequests = _ctx.InfoRequest
+                    .Include(info => info.Product)
+                        .ThenInclude(p => p.Brand)
+                    .Include(info => info.InfoRequestReplies)
+                        .ThenInclude(reply => reply.Account)
+                    .Include(info => info.Nation)
 
-                                .Select(info=> new RequestDetail
-                                {
-                                    Id=info.Id,
-                                    Name = info.Name+" "+info.LastName,
-                                    Email=info.Email,
-                                    Address=info.City+"("+info.PostalCode+"),"+info.Nation.Name,
-                                    Product=new PolishedProduct
-                                    {
-                                        ProductId=info.ProductId,
-                                        ProductName=info.Product.Name,
-                                        Brandname=info.Product.Brand.BrandName,
-                                    },
-                                    Replies=info.InfoRequestReplies.OrderByDescending(info=>info.InsertedDate)
-                                    .Select(rep=>new APIReply
-                                    {
-                                        Id=rep.Id,
-                                        Name=rep.Account.AccountType==1?info.Product.Brand.BrandName:info.Name+" "+info.LastName,
-                                        ReplyText=rep.ReplyText
-                                    }),
-                                   
-                                })
-                                .Where(info=>info.Id==id);
+                    .Select(info => new RequestDetail
+                    {
+                        Id = info.Id,
+                        Name = info.Name + " " + info.LastName,
+                        Email = info.Email,
+                        Address = info.City + "(" + info.PostalCode + ")," + info.Nation.Name,
+                        Product = new PolishedProduct
+                        {
+                            ProductId = info.ProductId,
+                            ProductName = info.Product.Name,
+                            Brandname = info.Product.Brand.BrandName,
+                        },
+                        Replies = info.InfoRequestReplies.OrderByDescending(info => info.InsertedDate)
+                        .Select(rep => new APIReply
+                        {
+                            Id = rep.Id,
+                            Name = rep.Account.AccountType == 1 ? info.Product.Brand.BrandName : info.Name + " " + info.LastName,
+                            ReplyText = rep.ReplyText
+                        }),
+
+                    })
+                    .Where(info => info.Id == id);
 
 
-            return Ok(InfoRequests);
+                return Ok(InfoRequests);
+            }
         }
 
-        [HttpGet("Test/{id}")]
-        public IActionResult Test(int id)
+        /// <summary>
+        /// returns a Brand with relative view requests, taking as input an Id. this method uses a different query linq
+        /// </summary>
+        [HttpGet("AlternativeBrandDetail/{id}")]
+        public IActionResult AlternativeBrandDetail(int id)
         {
 
             var Categs = _ctx.ProductCategories
